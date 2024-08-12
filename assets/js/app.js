f463997485e5ec86f1430ed723a00d922b0058de
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-08-12 17:59:03 +0200

    maj obsolette

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,24 @@
 
 // SWIPER
-
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    direction: getDirection(),
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    on: {
-      resize: function () {
-        swiper.changeDirection(getDirection());
+ var swiper = new Swiper('.swiper', {
+      slidesPerView: 3,
+      direction: getDirection(),
+     
+      navigation: {
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1',
       },
-    },
-  });
+    
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
 
-  function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
 
-    return direction;
-  }
+      return direction;
+    }
