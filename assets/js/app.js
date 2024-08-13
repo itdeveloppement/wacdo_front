8231dc51aa7fee019751b0252942a4cd43704f11
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-08-13 10:38:46 +0200

    affichage caroussel categorie et affichage liste produit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,17 @@
-// ----------------- CAROUSSEL -------------------------
+
+  
+  // ----------------- CAROUSSEL CATEGORIE -------------------------
 
 // selection des elements
-const carousel = document.querySelector('.carousel-inner');
+const carousel = document.querySelector('.carousel-categorie');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');  
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.cardCategorie');
 
 // variables
 let currentIndex = 0;
-const cardWidth = cards[0].offsetWidth; // calcul du deplacement d'une card
+//  const cardWidth = cards[0].offsetWidth; // calcul du deplacement d'une card
+cardWidth = 200; // largeur card
 
 // bornage du defilement
 const isFirstSlide = () => currentIndex === 0; // si index card = 0 (premiere card) alors true sinon false
@@ -54,3 +57,141 @@ const goToNextSlide = () => {
 // ecouteurs evenements
 prevBtn.addEventListener('click', goToPrevSlide);
 nextBtn.addEventListener('click', goToNextSlide);
+
+// ---------- AFFICHAGE CARDS CATEGORIE --------------------------
+/**
+ * role : afficher les cards categorie
+ * param : {array} tableau de données
+ * return : no
+ */
+function afficherCardsCategorie(datas){
+  let zone = document.querySelector(".carousel-categorie") // ciblage
+  let template = ''; // declaration
+  // extraction des données et construction card / template
+  console.log(datas)
+  datas.forEach(card=>{
+      template += 
+      `
+      <article class="cardCategorie">
+        <div>
+          <div>
+              <img src="../images/${card.image}" alt="menu">
+          </div>
+          <p>${card.title}</p>
+        </div>
+      </article>
+      `;    
+  });  
+  
+  // ajout au html
+  zone.innerHTML = template;
+
+  // --------- ECOUTEUR EVENEMENT CARD CATEGORIE apres ajout du DOM template------------------
+const cardsCategorie = document.querySelectorAll('.cardCategorie');
+cardsCategorie.forEach(card => {
+  card.addEventListener('click', (event) => {
+    const categorie = event.currentTarget.querySelector('p').textContent;
+   
+    datasProduits(categorie);
+  });
+});
+
+}
+
+// ---------- AFFICHAGE CARDS PRODUITS --------------------------
+/**
+ * role : afficher les cards produits
+ * param : {array} tableau de données
+ * return : no
+ */
+function afficherCardsProduit(datas, categorie){
+
+console.log(categorie)
+console.log(datas[categorie])
+
+
+  let zone = document.querySelector(".listeProduits")
+  // let templateTitre = '';
+
+  let template= ''; 
+  datas[categorie].forEach(card=>{
+console.log(card.image)
+      template += 
+      `
+      <article class="cardProduit">
+        <div>
+          <div>
+              <img src="../images/${card.image}" alt="menu">
+          </div>
+          <p>${card.nom}</p>
+          <p>${card.prix} €</p>
+        </div>
+      </article>
+      `;    
+  });  
+ 
+  // ajout au html
+  zone.innerHTML = template;
+  
+}
+
+// ------- FORM CHEVALET ----------------------------------------------
+/*
+const form = document.getElementById('formChevalet');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    // Récupérer les valeurs des champs
+    const nombre1 = document.getElementById('nombre1').value;
+    const nombre2 = document.getElementById('nombre2').value;
+    const nombre3 = document.getElementById('nombre3').value;  
+
+    // Envoyer les données à la nouvelle page (par exemple, en utilisant l'URLSearchParams)
+    const params = new URLSearchParams();
+    params.append('nombre1', nombre1);
+    params.append('nombre2', nombre2);
+    params.append('nombre3', nombre3);
+
+    window.location.href = 'nouvelle_page.html?' + params.toString();
+});
+*/
+// ---------------------- FETCHE ------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  fetch('../../json/categories.json')
+  .then(response => {
+    return response.json();
+  })
+  .then (datas => {
+    afficherCardsCategorie(datas)
+  })
+  .catch(error => {
+    // Gérer les erreurs
+    console.error('Erreur lors de la recuperation de la liste des categories :', error);
+  });
+
+
+});
+
+/**
+ * role : recuperation de la liste des produits
+ * param : no
+ * return :array : liste des produits
+ */
+function datasProduits(categorie) { 
+  fetch('../../json/produits.json')
+  .then(response => {
+    return response.json();
+  })
+  .then (datas => {
+    afficherCardsProduit(datas, categorie)
+  })
+  .catch(error => {
+    // Gérer les erreurs
+    console.error('Erreur lors de la recuperation de la liste des categories :', error);
+  });
+
+}
+  
