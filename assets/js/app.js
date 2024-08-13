0264e9a2739e5d44b6b2846e69a0230e97a4675c
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-08-13 11:57:10 +0200

    form chevalet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,122 @@
+// -------------- AU CHARGEMENT --------------------
+document.addEventListener('DOMContentLoaded', () => {
+  const numeroChevalet = '';
+  const currentUrl = window.location.pathname; // url relative
 
-  
-  // ----------------- CAROUSSEL CATEGORIE -------------------------
-
-// selection des elements
-const carousel = document.querySelector('.carousel-categorie');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');  
-const cards = document.querySelectorAll('.cardCategorie');
-
-// variables
-let currentIndex = 0;
-//  const cardWidth = cards[0].offsetWidth; // calcul du deplacement d'une card
-cardWidth = 200; // largeur card
-
-// bornage du defilement
-const isFirstSlide = () => currentIndex === 0; // si index card = 0 (premiere card) alors true sinon false
-const isLastSlide = () => currentIndex === cards.length - 1; // si index card = cards.length - 1; (derniere card) alors true sinon false
-
-/**
- * role : fonction pour defiler les cards/ Translation d'une longueur egale à la positon de la card * la largeur de la card
- * param : no
- * return : no 
- */
-const updateCarousel = () => {
-  carousel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-  nextBtn.disabled = isLastSlide(); // Désactiver le bouton "Suivant" à la fin
-  prevBtn.disabled = isFirstSlide(); // Désactiver le bouton "Précédent" au début (optionnel)
-};
-
-/**
- * role : faire defiler ou pas le caroussel (precedent)
- * param : no
- * return : no
- * algo : au click si defilement possible decrementte l'index et effectue la translation
- */
-const goToPrevSlide = () => {
-  if (!isFirstSlide()) {
-    currentIndex--;
-    updateCarousel();
+  // page choix
+  if (currentUrl === '/assets/pages/choix.html') { 
+    datasCategorie();
+    carousselHeader ();
   }
-};
+ // page chevalet
+ if (currentUrl === '/assets/pages/chevalet.html') { 
+  formChevalet ();
+}
+});
+
+
+// ---------------------- FETCHE ------------------------------------
 
 /**
- * role : faire defiler ou pas le caroussel (precedent)
+ * role : recuperation de la liste des categorie
  * param : no
- * return : no
- * algo : au click si defilement possible incremente l'index et effectue la translation
+ * return :array : liste des produits
  */
-const goToNextSlide = () => {
-  if (!isLastSlide()) {
-    currentIndex++;
-    updateCarousel();
-  }
-};
+function datasCategorie() { 
+fetch('../../json/categories.json')
+  .then(response => {
+    return response.json();
+  })
+  .then (datas => {
+    afficherCardsCategorie(datas)
+  })
+  .catch(error => {
+    // Gérer les erreurs
+    console.error('Erreur lors de la recuperation de la liste des categories :', error);
+  });
+}
 
-// ecouteurs evenements
-prevBtn.addEventListener('click', goToPrevSlide);
-nextBtn.addEventListener('click', goToNextSlide);
+/**
+ * role : recuperation de la liste des produits
+ * param : no
+ * return :array : liste des produits
+ */
+function datasProduits(categorie) { 
+  fetch('../../json/produits.json')
+  .then(response => {
+    return response.json();
+  })
+  .then (datas => {
+    afficherCardsProduit(datas, categorie)
+  })
+  .catch(error => {
+    // Gérer les erreurs
+    console.error('Erreur lors de la recuperation de la liste des categories :', error);
+  });
+}
 
+
+// ----------------- CAROUSSEL CATEGORIE -------------------------
+
+function carousselHeader () {
+  // selection des elements
+  const carousel = document.querySelector('.carousel-categorie');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');  
+  const cards = document.querySelectorAll('.cardCategorie');
+
+  // variables
+  let currentIndex = 0;
+  //  const cardWidth = cards[0].offsetWidth; // calcul du deplacement d'une card
+  cardWidth = 200; // largeur card
+
+  // bornage du defilement
+  const isFirstSlide = () => currentIndex === 0; // si index card = 0 (premiere card) alors true sinon false
+  const isLastSlide = () => currentIndex === cards.length - 1; // si index card = cards.length - 1; (derniere card) alors true sinon false
+
+  /**
+   * role : fonction pour defiler les cards/ Translation d'une longueur egale à la positon de la card * la largeur de la card
+   * param : no
+   * return : no 
+   */
+  const updateCarousel = () => {
+    carousel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    nextBtn.disabled = isLastSlide(); // Désactiver le bouton "Suivant" à la fin
+    prevBtn.disabled = isFirstSlide(); // Désactiver le bouton "Précédent" au début (optionnel)
+  };
+
+  /**
+   * role : faire defiler ou pas le caroussel (precedent)
+   * param : no
+   * return : no
+   * algo : au click si defilement possible decrementte l'index et effectue la translation
+   */
+  const goToPrevSlide = () => {
+    if (!isFirstSlide()) {
+      currentIndex--;
+      updateCarousel();
+    }
+  };
+
+  /**
+   * role : faire defiler ou pas le caroussel (precedent)
+   * param : no
+   * return : no
+   * algo : au click si defilement possible incremente l'index et effectue la translation
+   */
+  const goToNextSlide = () => {
+    if (!isLastSlide()) {
+      currentIndex++;
+      updateCarousel();
+    }
+  };
+
+  // ecouteurs evenements
+  prevBtn.addEventListener('click', goToPrevSlide);
+  nextBtn.addEventListener('click', goToNextSlide);
+}
 // ---------- AFFICHAGE CARDS CATEGORIE --------------------------
+
 /**
  * role : afficher les cards categorie
  * param : {array} tableau de données
@@ -68,7 +126,6 @@ function afficherCardsCategorie(datas){
   let zone = document.querySelector(".carousel-categorie") // ciblage
   let template = ''; // declaration
   // extraction des données et construction card / template
-  console.log(datas)
   datas.forEach(card=>{
       template += 
       `
@@ -91,7 +148,6 @@ const cardsCategorie = document.querySelectorAll('.cardCategorie');
 cardsCategorie.forEach(card => {
   card.addEventListener('click', (event) => {
     const categorie = event.currentTarget.querySelector('p').textContent;
-   
     datasProduits(categorie);
   });
 });
@@ -99,44 +155,48 @@ cardsCategorie.forEach(card => {
 }
 
 // ---------- AFFICHAGE CARDS PRODUITS --------------------------
+
 /**
  * role : afficher les cards produits
  * param : {array} tableau de données
  * return : no
  */
 function afficherCardsProduit(datas, categorie){
-
-console.log(categorie)
-console.log(datas[categorie])
-
-
   let zone = document.querySelector(".listeProduits")
   // let templateTitre = '';
+  let templateTitre=
+  `
+    <div>
+      <h3>Nos ${categorie}</h3>
+      <p>Choississez nos ${categorie}</p>
+    </div>
+  `;
 
-  let template= ''; 
+  let cardPoduit= ''; 
   datas[categorie].forEach(card=>{
-console.log(card.image)
-      template += 
+    cardPoduit += 
       `
-      <article class="cardProduit">
-        <div>
+        <article class="cardProduit">
           <div>
-              <img src="../images/${card.image}" alt="menu">
+            <div>
+                <img src="../images/${card.image}" alt="menu">
+            </div>
+            <p>${card.nom}</p>
+            <p>${card.prix} €</p>
           </div>
-          <p>${card.nom}</p>
-          <p>${card.prix} €</p>
-        </div>
-      </article>
+        </article>
       `;    
   });  
  
   // ajout au html
-  zone.innerHTML = template;
+  zone.innerHTML = templateTitre + cardPoduit;
   
 }
 
 // ------- FORM CHEVALET ----------------------------------------------
-/*
+
+function formChevalet () {
+
 const form = document.getElementById('formChevalet');
 
 form.addEventListener('submit', (event) => {
@@ -147,51 +207,28 @@ form.addEventListener('submit', (event) => {
     const nombre2 = document.getElementById('nombre2').value;
     const nombre3 = document.getElementById('nombre3').value;  
 
-    // Envoyer les données à la nouvelle page (par exemple, en utilisant l'URLSearchParams)
+    numeroChevalet = nombre1+nombre2+nombre3;
+  
+    // Envoyer les données à la nouvelle page
     const params = new URLSearchParams();
-    params.append('nombre1', nombre1);
-    params.append('nombre2', nombre2);
-    params.append('nombre3', nombre3);
-
-    window.location.href = 'nouvelle_page.html?' + params.toString();
-});
-*/
-// ---------------------- FETCHE ------------------------------------
-
-document.addEventListener('DOMContentLoaded', () => {
- 
-  fetch('../../json/categories.json')
-  .then(response => {
-    return response.json();
-  })
-  .then (datas => {
-    afficherCardsCategorie(datas)
-  })
-  .catch(error => {
-    // Gérer les erreurs
-    console.error('Erreur lors de la recuperation de la liste des categories :', error);
-  });
-
-
+    params.append('nombreChevalet', numeroChevalet);
+    window.location.href = 'choix.html?' + params.toString();
 });
 
-/**
- * role : recuperation de la liste des produits
- * param : no
- * return :array : liste des produits
- */
-function datasProduits(categorie) { 
-  fetch('../../json/produits.json')
-  .then(response => {
-    return response.json();
-  })
-  .then (datas => {
-    afficherCardsProduit(datas, categorie)
-  })
-  .catch(error => {
-    // Gérer les erreurs
-    console.error('Erreur lors de la recuperation de la liste des categories :', error);
-  });
+function afficherNumeroChevalet (){
+
+  
+  console.log(numeroChevalet)
+
+  let zone = document.querySelector(".surplace")
+  console.log(zone)
+  let template=
+  `
+    <p>Numero Chevalet : ${numeroChevalet}</div>
+  `
+  zone.innerHTML = template;
+}
 
 }
+
   
